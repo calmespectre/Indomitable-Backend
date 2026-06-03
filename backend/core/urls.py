b8bbfd4e743f6a3cf3c91b73cad0319b98d48f33
calmespectre <gerrad.meitameisaruni@gmail.com>
2026-06-03 19:22:07 +0300

    fixed production hosts

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+
+def api_root(request):
+    return JsonResponse({"status": "success", "message": "Indomitable Backend is running!"})
+
 
 urlpatterns = [
+    path('', api_root),
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/orders/', include('orders.urls')),
